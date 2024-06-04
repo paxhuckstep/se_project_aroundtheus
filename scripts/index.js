e74ba7645d2c92dda79023a6145a9c1e84f08da4
BigGarrison <paxhuckstep@gmail.com>
2024-06-03 18:26:32 -0600

    title to link

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 const initalCards = [
     {
-    title: 'Yosemite Valley',
+    name: 'Yosemite Valley',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
 }, {
-    title: 'Lake Louise',
+    name: 'Lake Louise',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
 
 }, {
-    title: 'Bald Mountains',
+    name: 'Bald Mountains',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
 
 }, {
-    title: 'Latemar',
+    name: 'Latemar',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
 
 } , {
-    title: 'Vanoise National Park',
+    name: 'Vanoise National Park',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
 
 }, {
-    title: 'Lago de Braies',
+    name: 'Lago de Braies',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 
 }
