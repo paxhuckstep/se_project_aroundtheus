d69acea3493cdd323e0b22d5fee863906ff199a6
BigGarrison <paxhuckstep@gmail.com>
2024-06-03 18:27:43 -0600

    initiAl

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initalCards = [
+const initialCards = [
     {
     name: 'Yosemite Valley',
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
