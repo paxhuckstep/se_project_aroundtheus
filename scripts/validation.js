05a41f739d8392e51e432e6f909d68afd86012c0
BigGarrison <paxhuckstep@gmail.com>
2024-07-30 12:55:08 -0600

    rename classes plus vid stuff

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,8 +1,11 @@
 
 
 
-function showInputError (formEl, inputEl, options) {
-    const ErrorMessageEl = document.querySelector()
+function showInputError (formEl, inputEl, {inputErrorClass}) {
+    const ErrorMessageEl = formEl.querySelector('#' + inputEl.id + '-error');
+    inputEl.classlist.add(inputErrorClass);
+    ErrorMessageEl.textContent = inputEl.validationMessage;
+    ErrorMessageEl.classlist.add(errorClass);
 }
 
 function checkInputValidity(formEl, inputEl, options) {
@@ -13,12 +16,30 @@ function checkInputValidity(formEl, inputEl, options) {
     }
 }
 
+function toggleButtonState(inputEls, submitButton, {inactiveButtonClass}) {
+    const foundInvalid = false;
+    inputEls.foreach(input => [
+        if(inputEl.validity.valid) {
+            foundInvalid = true;
+        }
+    ]);
+
+    if(foundInvalid) {
+        submitButton.classList.add(inactiveButtonClass);
+        submitButton.disabled = true;
+    } else {
+        submitButton.classList.remove(inactiveButtonClass);
+        submitButton.disabled = false;
+    }
+}
+
 function setEventListeners(formEl, options) {
     const {inputSelector} = options;
     const inputEls = [...(formEl.querySelectorAll(inputSelector))];
     inputEls.forEach(inputEl => {
         inputEl.addEventListener('input', (e) => {
             checkInputValidity(formEl, inputEl, options)
+            toggleButtonState(inputEls, submitButton, options);
         });
     });
 }
@@ -32,7 +53,7 @@ formEl.addEventListener('submit', (e) => {
 });
 
 setEventListeners(formEl, options);
-
+ 
 
 
 });
@@ -41,9 +62,9 @@ setEventListeners(formEl, options);
 const config = {    
     formSelector: ".modal__form", //FIX THE REST TO MATCH OUR CSS CLASS NAMES
     inputSelector: ".modal__input",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible"}
+    submitButtonSelector: ".modal__save",
+    inactiveButtonClass: "modal__button_disabled",
+    inputErrorClass: "modal__input_type_error",
+    errorClass: "modal__error_visible"}
 
 enableValidation(config, 'julian');
