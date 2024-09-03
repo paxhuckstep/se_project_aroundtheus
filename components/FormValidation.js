fbc69d4bdf06cfeaf8d2cb22e7e57f99497b175c
BigGarrison <paxhuckstep@gmail.com>
2024-09-03 16:59:55 -0600

    30 minute mark

--- components/FormValidation.js
+++ components/FormValidation.js
@@ -10,9 +10,42 @@ class FormValidator {
         this._form = formElement;
     }
 
+    _showInputError (formEl, inputEl, { inputErrorClass, errorClass }) = {
+        const ErrorMessageEl = this._form.querySelector("#" + inputEl.id + "-error");
+        inputEl.classList.add(inputErrorClass);
+        ErrorMessageEl.textContent = inputEl.validationMessage;
+        ErrorMessageEl.classList.add(errorClass);
+    }
+
+
+//wtf do I do for toggleButtonState ??
+    _toggleButtonState (inputList, ButtonElement, inactiveButtonClass) {
+        if(hasInvalidInput(inputList)) {
+            ButtonElement.classList.add(inactiveButtonClass);
+            ButtonElement.classList.add(inactiveButtonClass);
+            ButtonElement.disabled = true;
+        } else {
+            ButtonElement.classList.remove(inactiveButtonClass);
+            ButtonElement.disabled = false;
+        }
+    }
+
+_hasInvalidInput () {
+
+}
+
+_checkInputValidity() {
+    if (!inputEl.validity.valid) {
+        showInputError(formEl, inputEl, options);
+      } else {
+        hideInputError(formEl, inputEl, options);
+      }
+    }
+}
+
     _setEventListeners () {
-        const inputList = Array.from(this.form.querySelectorAll(this._inputSelector));
-        const ButtonElement = this.form.querySelector(this._submitButtonSelector);
+        this._inputList = Array.from(this.form.querySelectorAll(this._inputSelector));
+        this._ButtonElement = this.form.querySelector(this._submitButtonSelector);
 
         inputList.forEach((inputElement) => {
             checkInputValidity(this.form, inputElement, rest);
