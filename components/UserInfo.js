ac42ae77a115d504d71eb85cea63eb86854894d9
BigGarrison <paxhuckstep@gmail.com>
2024-10-01 09:00:43 -0600

    edited componenents

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,10 +1,23 @@
-export default class UserInfo {
+import {
+    profileTitle,
+    profileDescription
+    profileTitleInput
+    profileDescriptionInput
+} from "../utils/constants";
+
+import PopupWithForm from "./PopupWithForm";import PopupWithForm from "./PopupWithForm";
+
+export default class UserInfo extends PopupWithForm {
     constructor ({ userName, userJob }) {
         this._name = userName;
         this._job = userJob;
     }
+
+
 getUserInfo() {
-const userInfo = {}
+const userInfo = {
+
+}
 // getting the user info
 return userInfo
 }
