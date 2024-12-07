545f95211837563269138324de9510631a59a1e0
BigGarrison <paxhuckstep@gmail.com>
2024-12-06 22:56:39 -0700

    fixed edit icon size on avatar button, added text updating after clicking submit on modals

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -4,14 +4,20 @@ export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
     this._yesDeleteButton = document.querySelector("#delete-card-button");
+    this._saveButton = this._popupElement.querySelector(".modal__save");
   }
 
 setSubmitAction(action) {
     this._handleSubmit = action;
 }
+openPopup() {
+    this._saveButton.textContent = "Yes";
+    super.openPopup();
+   }
 
   setEventListeners() {
     this._yesDeleteButton.addEventListener("click", () => {
+        this._saveButton.textContent = "Deleting...";
       this._handleSubmit();
     });
     super.setEventListeners();
