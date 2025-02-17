ad49fd27bf096336f47348cc291e44f0d42aeaff
BigGarrison <paxhuckstep@gmail.com>
2025-02-17 14:56:45 -0700

    initial commit hacker-rank

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,297 @@ import {
 } from "../utils/constants.js";
 import Popup from "../components/Popup.js";
 import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
+////////////////////////////////////////////////////////
+
+function twoArrays(k, A, B) {
+  const length = A.length;
+  const aMax = Math.max(...A);
+  const bMax = Math.max(...B);
+  let aSortCount = [];
+  let bSortCount = [];
+  let aSorted = [];
+  let bSorted = [];
+  let sumArray = [];
+
+  for (let i = 0; i < aMax + 1; i++) {
+    aSortCount.push(0);
+  }
+  for (let i = 0; i < bMax + 1; i++) {
+    bSortCount.push(0);
+  }
+
+  for (let i = 0; i < length; i++) {
+    aSortCount[A[i]]++;
+    bSortCount[B[i]]++;
+  }
+  for (let i = 0; i < aMax + 1; i++) {
+    if (aSortCount[i] > 0) {
+      for (let j = 0; j < aSortCount[i]; j++) {
+        aSorted.push(i);
+      }
+    }
+  }
+  for (let i = bMax; i >= 0; i--) {
+    if (bSortCount[i] > 0) {
+      for (let j = 0; j < bSortCount[i]; j++) {
+        bSorted.push(i);
+      }
+    }
+  }
+  for (let i = 0; i < length; i++) {
+    sumArray.push(aSorted[i] + bSorted[i]);
+  }
+
+  for (let i = 0; i < length; i++) {
+    if (sumArray[i] < k) {
+      return "NO";
+    }
+  }
+  return "YES";
+}
+
+console.log(twoArrays(7, [2, 0, 1, 2, 2, 2, 99], [7, 99, 9, 0, 5, 6, 3]));
+
+// function countingSort(arr) {
+//   let outputArr = [];
+//   for (let i = 0; i < 100; i++) {
+//     outputArr.push(0);
+//   }
+//   arr.forEach((number) => {
+//     outputArr[number]++;
+//   });
+//   return outputArr;
+// }
+
+// function twoArraysOriginal(k, A, B) {
+//   A.sort();
+//   B.sort().reverse();
+//   let sumArr = [];
+//   for (let i = 0; i < A.length; i++) {
+//       sumArr[i] = A[i] + B[i];
+//   }
+//   for (let i = 0; i < sumArr.length; i++) {
+//       if (sumArr[i] < k) {
+//           return "NO";
+//       }
+
+//   }
+//   return "YES";
+// }
+
+// let Rone = [];
+// let Rtwo = [];
+
+// for (let i = 0; i < 700; i++) {
+//   Rone.push(i);
+//   Rtwo.push(800 - i);
+// }
+// // Rtwo.reverse();
+// // console.log( Rtwo);
+// console.log(twoArrays(900, Rone, Rtwo));
+
+// const firstArray = [1,2,3,4,5];
+// console.log(firstArray);
+// let secondArray = firstArray.concat().reverse();
+// console.log(firstArray);
+// secondArray.pop();
+// console.log(firstArray);
+// console.log(firstArray, secondArray);
+
+// function pangrams(s) {
+//   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//   const alphabetArray = alphabet.split("");
+//   const lowerCase = s.toLowerCase();
+//   let missersArr = [];
+//   alphabetArray.forEach((letter) => {
+//     if(!lowerCase.includes(letter)) {
+//       missersArr.push(letter);
+//     }
+//   })
+//   if (missersArr.length > 0) {
+//     const missersStr = missersArr.join(", ");
+//     return `"${s}" is not a pangram. "${s}" is missing: ${missersStr}.`;
+
+//   }
+//   return "pangram";
+// }
+
+// console.log(
+//   pangrams(
+//     "pangramCDBEGHKtYZP fijowvyuq"
+//   )
+// );
+
+// function miniMaxSum(arr) {
+//   let total = 0;
+//   arr.forEach((num) => {
+//     total = total + num;
+//   });
+//   const minSum = total - Math.max(...arr);
+//   const maxSum = total - Math.min(...arr);
+//   console.log(minSum, maxSum);
+// }
+
+// const stuff = [1, 2, 3, 4, 5];
+
+// console.log(miniMaxSum(stuff));
+
+// function arrayReverse(arr) {
+//   let newArr = []
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   newArr.unshift(arr[i]);
+//   // }
+//   arr.forEach((piece) => {
+//     newArr.unshift(piece);
+//   })
+//   return newArr
+//   // return arr.reverse();
+// }
+
+// const rev = [1, 2, 4, 5, 9, 8];
+
+// console.log(arrayReverse(rev));
+
+// function timeConversion(s) {
+//   console.log(s);
+//   const wholeArray = s.split("");
+//   wholeArray.pop();
+//   wholeArray.pop();
+//   const noLetters = wholeArray.join("");
+//   const timeArray = noLetters.split(":");
+
+//   if (s.charAt(0) === "1" && s.charAt(1) === "2") {
+//     if (s.charAt(8) === "A") {
+//       timeArray[0] = "00";
+//       return timeArray.join(":");
+//     }
+//     if (s.charAt(8) === "P") {
+//       return noLetters;
+//     }
+//   }
+
+//   if (s.charAt(8) === "P") {
+//     timeArray[0] = (Number(timeArray[0]) + 12).toString();
+//     return timeArray.join(":");
+//   }
+
+//   return noLetters;
+// }
+
+// console.log(timeConversion("12:11:11AM"));
+
+// function timeConversionOriginal(s) {
+//   const wholeArray = s.split("");
+//   wholeArray.pop();
+//   wholeArray.pop();
+//   const noLetters = wholeArray.join("");
+//   const timeArray = noLetters.split(":");
+
+//   if (s.charAt(8) === "P" && s.charAt(0) === "1" && s.charAt(1) === "2") {
+//     return noLetters;
+//   }
+
+//   if (s.charAt(8) === "P") {
+//     timeArray[0] = (Number(timeArray[0]) + 12).toString();
+//     return timeArray.join(":");
+//   }
+
+//   if (s.charAt(8) === "A" && s.charAt(0) === "1" && s.charAt(1) === "2") {
+//     timeArray[0] = "00";
+//     return timeArray.join(":");
+//   }
+
+//   return noLetters;
+// }
+
+// function countingSort(arr) {
+//   let outputArr = [];
+//   for (let i = 0; i < 100; i++) {
+//     outputArr.push(0);
+//   }
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   // outputArr[arr[i]] = outputArr[arr[i]] + 1;
+//   //   outputArr[arr[i]]++
+//   // }
+//   arr.forEach((number) => {
+//     outputArr[number]++;
+//   });
+//   return outputArr;
+// }
+// const unsorted = [1, 2, 3, 5, 3, 2, 5, 4, 6, 7, 3, 99, 99, 99, 99];
+
+// console.log(countingSort(unsorted));
+
+// function matchingStrings(strings, queries) {
+//   const answer = [];
+//   queries.forEach((query) => {
+//     let matches = 0;
+//     strings.forEach((string) => {
+//       if (string === query) {
+//         matches++;
+//       }
+//     });
+//     answer.push(matches);
+//   });
+//   return answer;
+// }
+
+// const strs =["abcde",
+//   "sdaklfj",
+// "  asdjf",
+//   "na",
+//   "basdn",
+//   'sdaklfj',
+//   "asdjf",
+//   "na",
+//   "asdjf",
+//   "na",
+//   "basdn",
+//   "sdaklfj"
+//   ,"asdjf"]
+
+//   const qrs = ["abcde",
+//     "sdaklfj",
+//     "asdjf",
+//     "na",
+//     "basdn"]
+
+// console.log(matchingStrings(strs, qrs))
+
+// function flippingBits(n) {
+//   const bitStr = (n.toString(2));
+//   let preZeros = [];
+//   for (let i = 0; i < 32 - bitStr.length; i++) {
+//     preZeros.push("0");
+//   }
+//   const bitList = preZeros.concat(bitStr.split(""));
+//   const flipList = bitList.map((bit) => {
+//     if(bit === "1") {
+//       return "0"
+//     }
+//     return "1"
+//   })
+//   return parseInt(flipList.join(""), 2);
+
+//   }
+
+// console.log(flippingBits(4294967263));
+
+// function sockMerchant(ar) {
+//   const sorted = ar.sort();
+//   let matches = 0;
+//   for (let i = 0; i < ar.length; i++) {
+//     if (sorted[i] === sorted[i + 1]) {
+//       matches++;
+//       i++;
+//     }
+//   }
+//   return matches;
+// }
+
+// console.log(sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20]));
+
+//////////////////////////////////////////////////////////////////
 
 function createCard(cardData) {
   const cardElement = new Card(
